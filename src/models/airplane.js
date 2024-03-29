e0dc7bb16f9740ba2499b33913eee3e37748321b
Manoj Kumar <manoj8894421801@gmail.com>
2024-03-29 20:56:39 +0530

    Added: flights model with foreign key defined during model creation

--- src/models/airplane.js
+++ src/models/airplane.js
@@ -10,6 +10,11 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
+      this.hasMany(models.Flight, {
+        foreignKey: 'airplaneId',
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+      })
     }
   }
   Airplane.init({
--- src/models/airport.js
+++ src/models/airport.js
@@ -14,6 +14,14 @@ module.exports = (sequelize, DataTypes) => {
         onDelete: 'CASCADE',
         onUpdate: 'CASCADE'
       });
+
+      this.hasMany(models.Flight, {
+        foreignKey: 'departureAirportId'
+      });
+
+      this.hasMany(models.Flight, {
+        foreignKey: 'arrivalAirportId',
+      });
     }
   }
   Airport.init({
